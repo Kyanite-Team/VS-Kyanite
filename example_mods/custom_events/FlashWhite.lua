20483bc7568be16aa95b4f5711677a6e1fc5a7b1
VideoBot <lander.meersman@gmail.com>
2026-05-17 12:22:13 +0200

    woops

--- example_mods/custom_events/FlashWhite.lua
+++
@@ -1,6 +0,0 @@
-function onEvent(name, value1, value2)
-   if name == 'FlashWhite' then
-    cameraFlash('camGame', 'ffffff', value1, true)
-	
-	end
-end
--- example_mods/scripts/miss.lua
+++
@@ -1,3 +0,0 @@
-function noteMiss(membersIndex, noteData, noteType, isSustainNote)
-    triggerEvent('Play Animation', 'sad', 'GF')
-end
